1ffefd9ab8209e2e3f02b5efa628c89ad86ade77
kcarson97 <99177599+kcarson97@users.noreply.github.com>
2022-02-28 10:56:09 +0000

    Add files via upload

---
+++ kafka-practice/kafka-practice/kafka-practice/kafka-practice/src/main/java/com/KafkaPractice/kafkapractice/DataGenerator/DataGenerator.java
@@ -0,0 +1,74 @@
+package com.KafkaPractice.kafkapractice.DataGenerator;
+
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@Configuration
+public class DataGenerator {
+
+    //class capable of generating fake data
+    private Faker faker = new Faker();
+
+    /*
+    Generates a fake quote
+     */
+    public String quoteFaker(){
+
+        Map<String,Object> quoteData = new HashMap<>();
+
+        quoteData.put("time",new SimpleDateFormat("yyyy.MM.dd_HH:mm:ss").format(Calendar.getInstance().getTime()));
+        quoteData.put("sym",faker.stock().nyseSymbol());
+        quoteData.put("bid",faker.number().randomDouble(2,0,100));
+        quoteData.put("ask",faker.number().randomDouble(2, (int) ((double) quoteData.get("bid")),100));
+        quoteData.put("bsize", faker.number().numberBetween(0,200));
+        quoteData.put("asize", faker.number().numberBetween(0,200));
+        quoteData.put("mode",randomValue());
+        quoteData.put("ex", randomValue(new String [] {"N","O"}));
+        quoteData.put("src",faker.company().name().toUpperCase());
+
+       return quoteData.toString();
+    }
+
+    /*
+    Generates a fake trade
+     */
+    public String tradeFaker(){
+
+        Map<String,Object> tradeData = new HashMap<>();
+
+        tradeData.put("time",new SimpleDateFormat("yyyy.MM.dd_HHmmss").format(Calendar.getInstance().getTime()));
+        tradeData.put("sym",faker.stock().nyseSymbol());
+        tradeData.put("price",faker.number().randomDouble(2,0,100));
+        tradeData.put("size", faker.number().numberBetween(0,200));
+        tradeData.put("stop", faker.number().numberBetween(0,1));
+        tradeData.put("cond",randomValue());
+        tradeData.put("ex", randomValue(new String [] {"N","O"}));
+        tradeData.put("side", randomValue(new String [] {"sell","buy"}));
+
+        return tradeData.toString();
+    }
+
+    /*
+    Generates random number
+     */
+
+    public String randomValue(Object[] arr){
+        Random random = new Random();
+        return arr[random.nextInt(arr.length)].toString();
+    }
+
+    /*
+    Generates random character
+     */
+
+    public char randomValue(){
+        Random random = new Random();
+        return Character.toUpperCase((char) (random.nextInt(26) + 'a')) ;
+    }
+
+
+
+}
---
+++ kafka-practice/kafka-practice/kafka-practice/kafka-practice/src/main/java/com/KafkaPractice/kafkapractice/DataGenerator/Message.java
@@ -0,0 +1,50 @@
+package com.KafkaPractice.kafkapractice.DataGenerator;
+
+public class Message extends DataGenerator {
+
+    /*
+    The topic the message should be posted to
+     */
+    private String topic;
+
+    /*
+    The rate at which the data should be published
+     */
+    private int rate;
+    /*
+    The duration for which data should be published
+     */
+    private int duration;
+
+    public Message(String topic, int rate,int time){
+        this.topic=topic;
+        this.rate=rate;
+        this.duration=time;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    /*
+    Generates a fake quote/trade and returns it in string format
+     */
+    public String generateData(){
+
+        if(topic.equalsIgnoreCase("quote")){
+            return quoteFaker();
+        }else if(topic.equalsIgnoreCase("trade")){
+            return tradeFaker();
+        }else{
+            return null;
+        }
+    }
+}
